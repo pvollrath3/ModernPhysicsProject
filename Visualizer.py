3385b90fc2a2bde2e6090ad1f5b613a155a201e9
bperez40 <bperez40@gatech.edu>
2022-04-06 22:13:37 -0400

    Update again

--- Visualizer.py
+++ Visualizer.py
@@ -8,6 +8,7 @@ data = f.text
 current_text = ""
 p_array = [] # Distance to the origin in meters
 th_array = [] # Polar angle in radians
+az_array = [] # Azimuthal angle in radians
 prob_dens = [] # 2D array, each probability density is indexed by a radius and an angle theta
 counter = 0
 for c in data:
@@ -50,8 +51,29 @@ def sph_to_cart(p, the, phi):
     result = vector(ansx,ansy,ansz) # Result is vector in x,y,z
     return result
 
+def visualize_sphere(in_p, in_prob): # Pretty meh right now
+    for k in range(360):
+        for i in range(360):
+            for j,jval in enumerate(in_p):
+                electron = sphere(pos=sph_to_cart(in_p[j], pi*k/180, pi*k/180), radius = 0.5*10**-16, opacity = in_prob[i])
+            
+            
+# Older version of cylindrical visualizer
+'''
+def visualize_cylinder(in_p, in_th, in_prob): # Visualization for cylindrically symmetric orbitals with prob_dens input
+    for k in range(360): # Basically ends up rotating the pattern, probability density is cylindrically symmetric
+        for i, ival in enumerate(in_th): # For loop for array of theta angles
+            for j,jval in enumerate(in_p): # For loop for array of distances to the origin
+                electron = sphere(pos=sph_to_cart(in_p[j], in_th[i], pi*k/180), radius = 0.5*10**-16, opacity = in_prob[i])
+'''
 
-for k in range(360): # Basically ends up rotating the pattern, probability density is spherically symmetric
-    for i, ival in enumerate(th_array): # For loop for array of theta angles
-        for j,jval in enumerate(p_array): # For loop for array of distances to the origin
-            electron = sphere(pos=sph_to_cart(p_array[j], th_array[i], pi*k/180), radius = 0.5*10**-16, opacity = prob_dens[i])
+# Visualization for cylindrically symmetric orbitals with prob_dens input. Assumes there is a value for p, th, and prob for each row in the csv table
+def visualize_cylinder(in_p, in_th, in_prob):
+    for k in range(360): # Basically ends up rotating the pattern, probability density is cylindrically symmetric
+        for i, ival in enumerate(in_th): # For loop for array of theta angles
+            electron = sphere(pos=sph_to_cart(in_p[i], in_th[i], pi*k/180), radius = 0.5*10**-16, opacity = in_prob[i])
+
+#def visualize_general(in_p, in_th, in_az):
+
+
+visualize_cylinder(p_array, th_array, prob_dens)
